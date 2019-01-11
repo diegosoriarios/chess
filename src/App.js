c785a9d09be486fabbc200eecc8c8a1c74e06d6c
diego.rios <diego.rios@CTNAPPLE01YG8WN.local>
2019-01-11 21:16:39 -0200

    Movments

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         ' ',' ',' ',' ',' ',' ',' ',' ',
         ' ',' ',' ',' ',' ',' ',' ',' ',
         ' ',' ',' ',' ',' ',' ',' ',' ',
-        ' ',' ','b',' ',' ',' ',' ',' ',
+        ' ',' ',' ',' ',' ',' ',' ',' ',
         'P','P','P','P','P','P','P','P',
         'T','C','B','K','Q','B','C','T',
       ],
@@ -257,11 +257,241 @@ class App extends Component {
           mesa,
           selected: pos
         })
-        return '♝'
+        break;
       case 'q':
-        return '♛'
+        coluna = pos % 8;
+        linha = Math.floor(pos / 8);
+        beginLine = linha * 8;
+        endLine = beginLine + 7;
+        checkBegin = true;
+        checkEnd = true;
+        //checkHorizontal
+        for(i = 0; i < 8; i++){
+          if(pos-i >= beginLine && checkBegin){
+            if(mesa[pos-i] === ' '){
+              mesa[pos-i] = 'x'
+            }else{
+              if(mesa[pos-i] === 'T' || mesa[pos-i] === 'C' || mesa[pos-i] === 'B' || mesa[pos-i] === 'Q' || mesa[pos-i] === 'K' || mesa[pos-i] === 'P'){
+                mesa[pos-i] = 'x'
+                checkBegin = false;
+              }else{
+                if(mesa[pos-i] !== ' ' && mesa[pos-i] !== peca){
+                  console.log(i)
+                  checkBegin = false;
+                }
+              }
+            }
+          }
+          console.log(`Pos+i${pos+i}`)
+          console.log(endLine)
+          if(pos+i <= endLine && checkEnd){
+            if(mesa[pos+i] === ' '){
+              mesa[pos+i] = 'x'
+            }else{
+              if(mesa[pos+i] === 'T' || mesa[pos+i] === 'C' || mesa[pos+i] === 'B' || mesa[pos+i] === 'Q' || mesa[pos+i] === 'K' || mesa[pos+i] === 'P'){
+                mesa[pos+i] = 'x'
+                checkEnd = false;
+              }else{
+                if(mesa[pos+i] !== ' ' && mesa[pos+i] !== peca){
+                  console.log(i)
+                  checkEnd = false;
+                }
+              }
+            }
+          }
+        }
+        //checkVertical
+        checkBegin = true;
+        checkEnd = true;
+        for(i = 1; i <= 8; i++){
+          if(pos-(8*i) >= linha && checkBegin){
+            if(mesa[pos-(8*i)] === ' '){
+              mesa[pos-(8*i)] = 'x'
+            }else{
+              if(mesa[pos-(8*i)] === 'T' || mesa[pos-(8*i)] === 'C' || mesa[pos-(8*i)] === 'B' || mesa[pos-(8*i)] === 'Q' || mesa[pos-(8*i)] === 'K' || mesa[pos-(8*i)] === 'P'){
+                mesa[pos-(8*i)] = 'x'
+                checkBegin = false;
+              }else{
+                if(mesa[pos-(8*i)] !== ' ' && mesa[pos-(8*i)] !== peca){
+                  console.log(i)
+                  checkBegin = false;
+                }
+              }
+            }
+          }
+          if(pos+(8*i) <= 64 && checkEnd){
+            if(mesa[pos+(8*i)] === ' '){
+              mesa[pos+(8*i)] = 'x'
+            }else{
+              if(mesa[pos+(8*i)] === 'T' || mesa[pos+(8*i)] === 'C' || mesa[pos+(8*i)] === 'B' || mesa[pos+(8*i)] === 'Q' || mesa[pos+(8*i)] === 'K' || mesa[pos+(8*i)] === 'P'){
+                mesa[pos+(8*i)] = 'x'
+                checkEnd = false;
+              }else{
+                if(mesa[pos+(8*i)] !== ' ' && mesa[pos-8*i] !== peca){
+                  console.log(i)
+                  checkEnd = false;
+                }
+              }
+            }
+          }
+        }
+        coluna = pos % 8;
+        linha = Math.floor(pos / 8);
+        beginLine = linha * 8;
+        endLine = beginLine + 7;
+        checkBegin = true;
+        checkEnd = true;
+        counter = 0;
+        counter2 = 0;
+        //checkHorizontal
+        console.clear()
+        for(i = 0; i < 8; i++){
+          console.log(coluna)
+          if(counter <= coluna && checkBegin){
+            if(mesa[pos-(9*i)] === ' '){
+              mesa[pos-(9*i)] = 'x'
+            }else{
+              if(mesa[pos-(9*i)] === 'T' || mesa[pos-(9*i)] === 'C' || mesa[pos-(9*i)] === 'B' || mesa[pos-(9*i)] === 'Q' || mesa[pos-(9*i)] === 'K' || mesa[pos-(9*i)] === 'P'){
+                mesa[pos-(9*i)] = 'x'
+                checkBegin = false;
+              }else{
+                if(mesa[pos-(9*i)] !== ' ' && mesa[pos-(9*i)] !== peca){
+                  console.log(i)
+                  checkBegin = false;
+                }
+              }
+            }
+            counter++;
+          }
+          if(counter2 < (8-coluna) && checkEnd){
+            if(mesa[pos+(9*i)] === ' '){
+              mesa[pos+(9*i)] = 'x'
+            }else{
+              if(mesa[pos+(9*i)] === 'T' || mesa[pos+(9*i)] === 'C' || mesa[pos+(9*i)] === 'B' || mesa[pos+(9*i)] === 'Q' || mesa[pos+(9*i)] === 'K' || mesa[pos+(9*i)] === 'P'){
+                mesa[pos+(9*i)] = 'x'
+                checkEnd = false;
+              }else{
+                if(mesa[pos+(9*i)] !== ' ' && mesa[pos+(9*i)] !== peca){
+                  console.log(i)
+                  checkEnd = false;
+                }
+              }
+            }
+            counter2++;
+          }
+        }
+        //checkVertical
+        checkBegin = true;
+        checkEnd = true;
+        counter = 0;
+        counter2 = 0;
+        for(i = 1; i <= 8; i++){
+          if(counter < 7-coluna && checkBegin){
+            if(mesa[pos-(7*i)] === ' '){
+              mesa[pos-(7*i)] = 'x'
+            }else{
+              if(mesa[pos-(7*i)] === 'T' || mesa[pos-(7*i)] === 'C' || mesa[pos-(7*i)] === 'B' || mesa[pos-(7*i)] === 'Q' || mesa[pos-(7*i)] === 'K' || mesa[pos-(7*i)] === 'P'){
+                mesa[pos-(7*i)] = 'x'
+                checkBegin = false;
+              }else{
+                if(mesa[pos-(7*i)] !== ' ' && mesa[pos-(7*i)] !== peca){
+                  checkBegin = false;
+                }
+              }
+            }
+            counter++;
+          }
+          if(counter2 < coluna && checkEnd){
+            if(mesa[pos+(7*i)] === ' '){
+              mesa[pos+(7*i)] = 'x'
+            }else{
+              if(mesa[pos+(7*i)] === 'T' || mesa[pos+(7*i)] === 'C' || mesa[pos+(7*i)] === 'B' || mesa[pos+(7*i)] === 'Q' || mesa[pos+(7*i)] === 'K' || mesa[pos+(7*i)] === 'P'){
+                mesa[pos+(7*i)] = 'x'
+                checkEnd = false;
+              }else{
+                if(mesa[pos+(7*i)] !== ' ' && mesa[pos+7*i] !== peca){
+                  checkEnd = false;
+                }
+              }
+            }
+            counter2++;
+          }
+        }
+        this.setState({
+          mesa,
+          selected: pos
+        })
+        break;
       case 'k':
-        return '♚'
+        if(pos === 56){
+          if(mesa[pos+1] === ' ' || mesa[pos+1] === 'T' || mesa[pos+1] === 'C' || mesa[pos+1] === 'B' || mesa[pos+1] === 'Q' || mesa[pos+1] === 'P' || pos+1 > 64){mesa[pos+1] = 'x';}
+          if(mesa[pos-8] === ' ' || mesa[pos-8] === 'T' || mesa[pos-8] === 'C' || mesa[pos-8] === 'B' || mesa[pos-8] === 'Q' || mesa[pos-8] === 'P' || pos-8 > 64){mesa[pos-8] = 'x';}
+          if(mesa[pos-7] === ' ' || mesa[pos-7] === 'T' || mesa[pos-7] === 'C' || mesa[pos-7] === 'B' || mesa[pos-7] === 'Q' || mesa[pos-7] === 'P' || pos-7 > 64){mesa[pos-7] = 'x';}
+        }else{
+          if(pos === 63){
+            if(mesa[pos-9] === ' ' || mesa[pos-9] === 'T' || mesa[pos-9] === 'C' || mesa[pos-9] === 'B' || mesa[pos-9] === 'Q' || mesa[pos-9] === 'P' || pos-9 > 64){mesa[pos-9] = 'x';}
+            if(mesa[pos-1] === ' ' || mesa[pos-1] === 'T' || mesa[pos-1] === 'C' || mesa[pos-1] === 'B' || mesa[pos-1] === 'Q' || mesa[pos-1] === 'P' || pos-1 > 64){mesa[pos-1] = 'x';}
+            if(mesa[pos-8] === ' ' || mesa[pos-8] === 'T' || mesa[pos-8] === 'C' || mesa[pos-8] === 'B' || mesa[pos-8] === 'Q' || mesa[pos-8] === 'P' || pos-8 > 64){mesa[pos-8] = 'x';}
+          }else{
+            if(pos === 0){
+              if(mesa[pos+9] === ' ' || mesa[pos+9] === 'T' || mesa[pos+9] === 'C' || mesa[pos+9] === 'B' || mesa[pos+9] === 'Q' || mesa[pos+9] === 'P' || pos+9 > 64){mesa[pos+9] = 'x';}
+              if(mesa[pos+1] === ' ' || mesa[pos+1] === 'T' || mesa[pos+1] === 'C' || mesa[pos+1] === 'B' || mesa[pos+1] === 'Q' || mesa[pos+1] === 'P' || pos+1 > 64){mesa[pos+1] = 'x';}
+              if(mesa[pos+8] === ' ' || mesa[pos+8] === 'T' || mesa[pos+8] === 'C' || mesa[pos+8] === 'B' || mesa[pos+8] === 'Q' || mesa[pos+8] === 'P' || pos+8 > 64){mesa[pos+8] = 'x';}
+            }else{
+              if(pos === 7){
+                if(mesa[pos+8] === ' ' || mesa[pos+8] === 'T' || mesa[pos+8] === 'C' || mesa[pos+8] === 'B' || mesa[pos+8] === 'Q' || mesa[pos+8] === 'P' || pos+8 > 64){mesa[pos+8] = 'x';}
+                if(mesa[pos+7] === ' ' || mesa[pos+7] === 'T' || mesa[pos+7] === 'C' || mesa[pos+7] === 'B' || mesa[pos+7] === 'Q' || mesa[pos+7] === 'P' || pos+7 > 64){mesa[pos+7] = 'x';}
+                if(mesa[pos-1] === ' ' || mesa[pos-1] === 'T' || mesa[pos-1] === 'C' || mesa[pos-1] === 'B' || mesa[pos-1] === 'Q' || mesa[pos-1] === 'P' || pos-1 > 64){mesa[pos-1] = 'x';}
+              }else{
+                if(pos % 8 === 7){
+                  if(mesa[pos+8] === ' ' || mesa[pos+8] === 'T' || mesa[pos+8] === 'C' || mesa[pos+8] === 'B' || mesa[pos+8] === 'Q' || mesa[pos+8] === 'P' || pos+8 > 64){mesa[pos+8] = 'x';}
+                  if(mesa[pos+7] === ' ' || mesa[pos+7] === 'T' || mesa[pos+7] === 'C' || mesa[pos+7] === 'B' || mesa[pos+7] === 'Q' || mesa[pos+7] === 'P' || pos+7 > 64){mesa[pos+7] = 'x';}
+                  if(mesa[pos-9] === ' ' || mesa[pos-9] === 'T' || mesa[pos-9] === 'C' || mesa[pos-9] === 'B' || mesa[pos-9] === 'Q' || mesa[pos-9] === 'P' || pos-9 > 64){mesa[pos-9] = 'x';}
+                  if(mesa[pos-1] === ' ' || mesa[pos-1] === 'T' || mesa[pos-1] === 'C' || mesa[pos-1] === 'B' || mesa[pos-1] === 'Q' || mesa[pos-1] === 'P' || pos-1 > 64){mesa[pos-1] = 'x';}
+                  if(mesa[pos-8] === ' ' || mesa[pos-8] === 'T' || mesa[pos-8] === 'C' || mesa[pos-8] === 'B' || mesa[pos-8] === 'Q' || mesa[pos-8] === 'P' || pos-8 > 64){mesa[pos-8] = 'x';}
+                }else{
+                  if(pos % 8 === 0){
+                    if(mesa[pos+9] === ' ' || mesa[pos+9] === 'T' || mesa[pos+9] === 'C' || mesa[pos+9] === 'B' || mesa[pos+9] === 'Q' || mesa[pos+9] === 'P' || pos+9 > 64){mesa[pos+9] = 'x';}
+                    if(mesa[pos+1] === ' ' || mesa[pos+1] === 'T' || mesa[pos+1] === 'C' || mesa[pos+1] === 'B' || mesa[pos+1] === 'Q' || mesa[pos+1] === 'P' || pos+1 > 64){mesa[pos+1] = 'x';}
+                    if(mesa[pos+8] === ' ' || mesa[pos+8] === 'T' || mesa[pos+8] === 'C' || mesa[pos+8] === 'B' || mesa[pos+8] === 'Q' || mesa[pos+8] === 'P' || pos+8 > 64){mesa[pos+8] = 'x';}
+                    if(mesa[pos-8] === ' ' || mesa[pos-8] === 'T' || mesa[pos-8] === 'C' || mesa[pos-8] === 'B' || mesa[pos-8] === 'Q' || mesa[pos-8] === 'P' || pos-8 > 64){mesa[pos-8] = 'x';}
+                    if(mesa[pos-7] === ' ' || mesa[pos-7] === 'T' || mesa[pos-7] === 'C' || mesa[pos-7] === 'B' || mesa[pos-7] === 'Q' || mesa[pos-7] === 'P' || pos-7 > 64){mesa[pos-7] = 'x';}
+                  }else{
+                    if(pos > 56){
+                      if(mesa[pos+1] === ' ' || mesa[pos+1] === 'T' || mesa[pos+1] === 'C' || mesa[pos+1] === 'B' || mesa[pos+1] === 'Q' || mesa[pos+1] === 'P' || pos+1 > 64){mesa[pos+1] = 'x';}
+                      if(mesa[pos-9] === ' ' || mesa[pos-9] === 'T' || mesa[pos-9] === 'C' || mesa[pos-9] === 'B' || mesa[pos-9] === 'Q' || mesa[pos-9] === 'P' || pos-9 > 64){mesa[pos-9] = 'x';}
+                      if(mesa[pos-1] === ' ' || mesa[pos-1] === 'T' || mesa[pos-1] === 'C' || mesa[pos-1] === 'B' || mesa[pos-1] === 'Q' || mesa[pos-1] === 'P' || pos-1 > 64){mesa[pos-1] = 'x';}
+                      if(mesa[pos-8] === ' ' || mesa[pos-8] === 'T' || mesa[pos-8] === 'C' || mesa[pos-8] === 'B' || mesa[pos-8] === 'Q' || mesa[pos-8] === 'P' || pos-8 > 64){mesa[pos-8] = 'x';}
+                      if(mesa[pos-7] === ' ' || mesa[pos-7] === 'T' || mesa[pos-7] === 'C' || mesa[pos-7] === 'B' || mesa[pos-7] === 'Q' || mesa[pos-7] === 'P' || pos-7 > 64){mesa[pos-7] = 'x';}
+                    }else{
+                      if(pos < 7){
+                        if(mesa[pos+9] === ' ' || mesa[pos+9] === 'T' || mesa[pos+9] === 'C' || mesa[pos+9] === 'B' || mesa[pos+9] === 'Q' || mesa[pos+9] === 'P' || pos+9 > 64){mesa[pos+9] = 'x';}
+                        if(mesa[pos+1] === ' ' || mesa[pos+1] === 'T' || mesa[pos+1] === 'C' || mesa[pos+1] === 'B' || mesa[pos+1] === 'Q' || mesa[pos+1] === 'P' || pos+1 > 64){mesa[pos+1] = 'x';}
+                        if(mesa[pos+8] === ' ' || mesa[pos+8] === 'T' || mesa[pos+8] === 'C' || mesa[pos+8] === 'B' || mesa[pos+8] === 'Q' || mesa[pos+8] === 'P' || pos+8 > 64){mesa[pos+8] = 'x';}
+                        if(mesa[pos+7] === ' ' || mesa[pos+7] === 'T' || mesa[pos+7] === 'C' || mesa[pos+7] === 'B' || mesa[pos+7] === 'Q' || mesa[pos+7] === 'P' || pos+7 > 64){mesa[pos+7] = 'x';}
+                        if(mesa[pos-1] === ' ' || mesa[pos-1] === 'T' || mesa[pos-1] === 'C' || mesa[pos-1] === 'B' || mesa[pos-1] === 'Q' || mesa[pos-1] === 'P' || pos-1 > 64){mesa[pos-1] = 'x';}
+                      }else{
+                        if(mesa[pos+9] === ' ' || mesa[pos+9] === 'T' || mesa[pos+9] === 'C' || mesa[pos+9] === 'B' || mesa[pos+9] === 'Q' || mesa[pos+9] === 'P' || pos+9 > 64){mesa[pos+9] = 'x';}
+                        if(mesa[pos+1] === ' ' || mesa[pos+1] === 'T' || mesa[pos+1] === 'C' || mesa[pos+1] === 'B' || mesa[pos+1] === 'Q' || mesa[pos+1] === 'P' || pos+1 > 64){mesa[pos+1] = 'x';}
+                        if(mesa[pos+8] === ' ' || mesa[pos+8] === 'T' || mesa[pos+8] === 'C' || mesa[pos+8] === 'B' || mesa[pos+8] === 'Q' || mesa[pos+8] === 'P' || pos+8 > 64){mesa[pos+8] = 'x';}
+                        if(mesa[pos+7] === ' ' || mesa[pos+7] === 'T' || mesa[pos+7] === 'C' || mesa[pos+7] === 'B' || mesa[pos+7] === 'Q' || mesa[pos+7] === 'P' || pos+7 > 64){mesa[pos+7] = 'x';}
+                        if(mesa[pos-9] === ' ' || mesa[pos-9] === 'T' || mesa[pos-9] === 'C' || mesa[pos-9] === 'B' || mesa[pos-9] === 'Q' || mesa[pos-9] === 'P' || pos-9 > 64){mesa[pos-9] = 'x';}
+                        if(mesa[pos-1] === ' ' || mesa[pos-1] === 'T' || mesa[pos-1] === 'C' || mesa[pos-1] === 'B' || mesa[pos-1] === 'Q' || mesa[pos-1] === 'P' || pos-1 > 64){mesa[pos-1] = 'x';}
+                        if(mesa[pos-8] === ' ' || mesa[pos-8] === 'T' || mesa[pos-8] === 'C' || mesa[pos-8] === 'B' || mesa[pos-8] === 'Q' || mesa[pos-8] === 'P' || pos-8 > 64){mesa[pos-8] = 'x';}
+                        if(mesa[pos-7] === ' ' || mesa[pos-7] === 'T' || mesa[pos-7] === 'C' || mesa[pos-7] === 'B' || mesa[pos-7] === 'Q' || mesa[pos-7] === 'P' || pos-7 > 64){mesa[pos-7] = 'x';}
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        this.setState({
+          mesa,
+          selected: pos
+        })
+        break;
       case 'p':
         mesa[pos+8] = 'x';
         mesa[pos+16] = 'x';
